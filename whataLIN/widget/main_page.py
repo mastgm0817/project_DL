90d1a200276fa242413ec495a9954370485c077b
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-04-12 16:25:21 +0900

    show

--- whataLIN/widget/main_page.py
+++ whataLIN/widget/main_page.py
@@ -167,14 +167,17 @@ def search_data(option, df):
 
     elif option == "column, row로 검색":
         try:
-            columns=(st.text_input("검색할 column을 입력하세요.")).replace(" ","")
-            if ',' in columns:
-                columns = columns.split(",")
-                if columns in df.columns:
-                    index_name = st.text_input('검색할 index를 입력해 주세요').replace(" ","")
-                    index_name = list(map(int, index_name.split(",")))
+            columns=(input("검색할 column을 입력하세요.")).replace(" ","").split(",")
+            # print(columns)
+            # columns = columns.split(",")
+            index_name = input('검색할 index를 입력해 주세요').replace(" ","")
+            index_name = list(map(int, index_name.split(",")))
+            show_df=df.loc[index_name, columns]
+            # print(index_name)
 
-            show_df = df[(df[columns] == index_name)]
+            # print(df.loc[index_name, columns])
             st.write(show_df)
+
         except:
             st.write("비정상적인 column 값입니다.")
+            # print("비정상적인 column 값입니다.")
