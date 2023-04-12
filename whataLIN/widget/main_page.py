9d9eebb3859ee3283430f91be9896d0d11d78eef
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-04-12 14:40:26 +0900

    if

--- whataLIN/widget/main_page.py
+++ whataLIN/widget/main_page.py
@@ -81,15 +81,17 @@ def explain_tab():
 
     option=["장르 비율 파이 차트", "장르 클러스터 산점도"]
     op_chart=st.selectbox("데이터 정보", option)
-    if op_chart=="장르 비율 파이 차트":
+
     # 파이 차트 표시
+    if op_chart=="장르 비율 파이 차트":
         labels = ['action', 'adventure', 'animmation', 'comedy', 'crime', 'drama', 'fantasy', 'horror', 'mystery', 'romance', 'sci-fi', 'short', 'thriler']
         values = [424, 238, 242, 667, 292, 829, 166, 354, 195, 342, 162, 201, 431]
         pie_chart(labels, values, title_text='Movie genre')
 
-    # 산점도 표시
-    fig = get_chart('whataLIN/scatterplot.pickle')
-    st.plotly_chart(fig)
+    elif op_chart=="장르 클러스터 산점도": 
+        # 산점도 표시
+        fig = get_chart('whataLIN/scatterplot.pickle')
+        st.plotly_chart(fig)
 
     # names = ['Trace A', 'Trace B', 'Trace C', 'Trace D', 'Trace E', 'Trace F', 'Trace G', 'Trace H', 'Trace I']
     # colors = ['red', 'blue', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
